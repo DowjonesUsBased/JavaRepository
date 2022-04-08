853ffd23ce5f49c946f633167d09ecf37b8b0ec6
shivam0651 <88841947+shivam0651@users.noreply.github.com>
2022-04-08 16:42:39 +0530

    new Expresion
    adding sir

--- LambdaExpression.java
+++ LambdaExpression.java
@@ -7,6 +7,7 @@ public class LambdaExpression {
 public static void main(String[] args) {
 	LambdaExpression l=new LambdaExpression();
 	System.out.println("Shivam Talwalkar");
+	System.out.println("vaibhav Sir");
 	
 }
 }
